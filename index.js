37b6e50e1a8f8a9286bd5d7a2a83cc0933bd92ad
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2025-07-08 13:27:06 +0100

    create animated receipt printer

---
+++ index.js
@@ -0,0 +1,53 @@
+const printIndicator = document.getElementById("print-indicator");
+const printIcon = document.getElementById("print-icon");
+const printButton = document.getElementById("print-button");
+const receiptWrapper = document.getElementById("receipt-wrapper");
+const receiptContainer = document.getElementById("receipt-container");
+const receiptsWrapper = document.getElementById("receipts-wrapper");
+
+const setPrintIndicator = (state) => {
+  printIndicator.classList.remove("active-background", "inactive-background");
+  printIcon.classList.remove("active-icon", "inactive-icon");
+  printIndicator.classList.add(`${state}-background`);
+  printIcon.classList.add(`${state}-icon`);
+};
+
+let isPrinting = false;
+let hasPrinted = false;
+
+setPrintIndicator("inactive");
+
+printButton.addEventListener("click", () => {
+  if (isPrinting) return;
+
+  isPrinting = true;
+
+  let newReceipt = null;
+
+  const printerSound = new Audio("./printer.mp3");
+  printerSound.play();
+
+  setPrintIndicator("active");
+
+  if (hasPrinted) {
+    newReceipt = receiptWrapper.cloneNode(true);
+    receiptsWrapper.appendChild(newReceipt);
+  } else {
+    receiptContainer.classList.add("print");
+    hasPrinted = true;
+  }
+
+  setTimeout(() => {
+    isPrinting = false;
+
+    if (newReceipt) {
+      newReceipt.classList.add("fade-out");
+
+      setTimeout(() => {
+        newReceipt.remove();
+      }, 5000);
+    }
+
+    setPrintIndicator("inactive");
+  }, 8000);
+});
